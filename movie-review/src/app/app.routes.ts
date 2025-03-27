ad3cd432d6245779cc3db3e9741166a36e656417
BirzhanArtykbay <157303167+BirzhanArtykbay@users.noreply.github.com>
2025-03-28 00:11:16 +0500

    Upgrade

--- movie-review/src/app/app.routes.ts
+++ movie-review/src/app/app.routes.ts
@@ -2,8 +2,9 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from "./components/home/home.component";
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
 import { LoginComponent } from "./components/login/login.component";
-import { AuthGuard } from "./auth.guard";
-import {MoviesComponent} from './components/movies/movies.component';
+import { AuthGuard } from "./auth.guard"; // Import the guard
+import { MoviesComponent } from './components/movies/movies.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -11,14 +12,13 @@ export const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
-    canActivate: [AuthGuard],
     children: [
       {
         path: 'dashboard',
         component: DashboardComponent,
-        canActivate: [AuthGuard],
         children: [
-          { path: 'movies', component: MoviesComponent }
+          { path: 'movies', component: MoviesComponent },  // Movies list page
+          { path: 'movies/:id', component: MovieDetailsComponent }  // Movie details page
         ]
       }
     ]
