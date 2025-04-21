47cfb692766ddf033e02b2fc1b7d86bdd62220bd
BirzhanArtykbay <157303167+BirzhanArtykbay@users.noreply.github.com>
2025-04-21 16:15:13 +0500

    back part

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,8 +1,22 @@
 from rest_framework import serializers
-from .models import Review
+from .models import Movie, Review, Genre
+from django.contrib.auth.models import User
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = '__all__'
         read_only_fields = ['owner']
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+class CustomMovieRatingSerializer(serializers.Serializer):
+    title = serializers.CharField()
+    average_rating = serializers.FloatField()
