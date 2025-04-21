b9fe7d7fa51a2bc516975d02420b39eaffa7130f
BirzhanArtykbay <157303167+BirzhanArtykbay@users.noreply.github.com>
2025-04-21 16:14:41 +0500

    back part

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ReviewViewSet
+from .views import (
+    MovieListView, MovieDetailView,
+    LoginView, MovieAverageRatingView, ReviewViewSet
+)
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -11,7 +14,11 @@ router = DefaultRouter()
 router.register(r'reviews', ReviewViewSet, basename='review')
 
 urlpatterns = [
-    path('tasks/', include(router.urls)),  
+    path('movies/', MovieListView.as_view()),
+    path('movies/<int:pk>/', MovieDetailView.as_view()),
+    path('movies/ratings/', MovieAverageRatingView.as_view()),
+    path('login/', LoginView.as_view()),
+    path('tasks/', include(router.urls)),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('logout/', TokenBlacklistView.as_view(), name='token_blacklist'),
