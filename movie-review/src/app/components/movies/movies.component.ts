5de76d28f5449f37f70c055764674d026aedfec1
BirzhanArtykbay <157303167+BirzhanArtykbay@users.noreply.github.com>
2025-04-22 02:13:57 +0500

    Update movies.component.ts

--- movie-review/src/app/components/movies/movies.component.ts
+++ movie-review/src/app/components/movies/movies.component.ts
@@ -1,41 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-import { MovieService} from '../../service/movie.service';
+import { MovieService } from '../../service/movie.service';
 import { Router } from '@angular/router';
-import {FormsModule} from '@angular/forms';
-import {NgForOf} from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
-  standalone:true,
-  imports: [FormsModule,NgForOf],
+  standalone: true,
+  imports: [FormsModule, NgForOf],
   styleUrls: ['./movies.component.css']
 })
 export class MoviesComponent implements OnInit {
   movies: any[] = [];
   filteredMovies: any[] = [];
-  searchQuery: string = "";
+  searchQuery: string = '';
+  useApiData: boolean = false;  // Toggle between API and local
 
-  constructor(private movieService: MovieService, private router: Router) {} // Inject Router
+  constructor(private movieService: MovieService, private router: Router) {}
 
   ngOnInit() {
     this.fetchMovies();
   }
 
   fetchMovies() {
-    this.movieService.getMovies().subscribe(data => {
-      this.movies = data;
-      this.filteredMovies = data;
-    });
+    if (this.useApiData) {
+      this.movieService.getApiMovies().subscribe(data => {
+        this.movies = data;
+        this.filteredMovies = data;
+      });
+    } else {
+      this.movieService.getLocalMovies().subscribe(data => {
+        this.movies = data;
+        this.filteredMovies = data;
+      });
+    }
+  }
+
+  toggleSource() {
+    this.useApiData = !this.useApiData;
+    this.fetchMovies();
   }
 
   searchMovies() {
     this.filteredMovies = this.movies.filter(movie =>
-      movie.Title.toLowerCase().includes(this.searchQuery.toLowerCase())
+      (movie.Title || movie.title).toLowerCase().includes(this.searchQuery.toLowerCase())
     );
   }
 
   goToMovieDetails(movie: any) {
-    this.router.navigate(['/dashboard/movies', movie.imdbID]);
+    const id = movie.imdbID || movie.id;
+    this.router.navigate(['/dashboard/movies', id]);
   }
 }
