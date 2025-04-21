91470d40ccea9e4add02e32c98a208f745058231
BirzhanArtykbay <157303167+BirzhanArtykbay@users.noreply.github.com>
2025-04-21 20:11:13 +0500

    Update urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import ReviewViewSet, MovieViewSet, GenreViewSet, user_list, basic_review_create, ProfileListAPIView, MovieByYearAPIView
-from .auth_views import LoginView, TokenBlacklistView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenBlacklistView,
+)
+from .views import (
+    ReviewViewSet,
+    MovieViewSet,
+    GenreViewSet,
+    user_list,
+    basic_review_create,
+    MovieListAPIView,
+    MovieByYearAPIView,
+)
+
 
 router = DefaultRouter()
 router.register(r'reviews', ReviewViewSet, basename='review')
@@ -10,13 +22,12 @@ router.register(r'movies', MovieViewSet, basename='movie')
 router.register(r'genres', GenreViewSet, basename='genre')
 
 urlpatterns = [
-    path('login/', LoginView.as_view()),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # You can switch to LoginView.as_view() if you want custom logic
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', TokenBlacklistView.as_view(), name='token_blacklist'),
     path('tasks/', include(router.urls)),
     path('users/', user_list),
     path('basic-review/', basic_review_create),
-    path('profiles/', ProfileListAPIView.as_view()),
+    path('movies-list/', MovieListAPIView.as_view(), name='movie-list'),
     path('movies-by-year/<int:year>/', MovieByYearAPIView.as_view()),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('logout/', TokenBlacklistView.as_view(), name='token_blacklist'),
 ]
