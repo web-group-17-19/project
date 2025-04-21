3592c618165e2ba0e3dea3b46a49a188e63ee8ad
BirzhanArtykbay <157303167+BirzhanArtykbay@users.noreply.github.com>
2025-04-21 23:09:17 +0500

    Update serializers.py

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,32 +1,32 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Review, Movie, Genre, Profile
+from .models import Review, Movie, Genre, Rating
 
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
         fields = ['id', 'name']
 
+class RatingSerializer(serializers.Serializer):
+    source = serializers.CharField()
+    value = serializers.CharField()
+
 class MovieSerializer(serializers.ModelSerializer):
-    genres = GenreSerializer(many=True, read_only=True)
+    ratings = RatingSerializer(many=True, read_only=True)
 
     class Meta:
         model = Movie
-        fields = ['id', 'title', 'description', 'release_date', 'genres']
+        fields = '__all__'
 
 class ReviewSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    movie = serializers.StringRelatedField()
 
     class Meta:
         model = Review
         fields = '__all__'
         read_only_fields = ['owner']
 
-class SimpleUserSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    email = serializers.EmailField()
-
 class BasicReviewSerializer(serializers.Serializer):
     text = serializers.CharField()
     rating = serializers.IntegerField()
+
