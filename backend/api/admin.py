2a0869a8e3e262bbb01f351274029a634d862f85
BirzhanArtykbay <157303167+BirzhanArtykbay@users.noreply.github.com>
2025-04-22 02:12:50 +0500

    Update admin.py

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from .models import Review
+from .models import Review, Movie
 
 admin.site.register(Review)
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'year', 'genre')
+    search_fields = ('title', 'genre', 'director')
+    list_filter = ('year', 'genre')
